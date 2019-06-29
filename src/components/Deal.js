f1e35a77cd73a0531e34afe214b414e6916276d8
You Song Hou <yousonghou@yahoo.com>
2019-06-28 23:26:52 -0400

    set up

---
+++ src/components/Deal.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {Text, View, Image} from 'react-native';
+
+export default class Deal extends Component{
+    constructor() {
+        super();
+        this.state = {
+          data: "",
+        };
+    }
+    componentDidMount() {
+        let url = `https://cnycserver.herokuapp.com/items/5d06d0c8f8af1d0004212e24`;
+        fetch(url)
+        .then(res => {
+            return res.json();
+        })
+        .then((data) => {
+            this.setState({
+              data: data.item,
+            });
+        })
+        .catch((err) => {
+            console.log('There was a problem with your fetch request' + err.message);
+        });
+      }
+  render() {
+    return (
+      <View>
+        <Text>{this.state.data.name}</Text>
+        <Image source={{uri: this.state.data.image}} style={{width: 193, height: 110}}/>
+        <Text>{this.state.data.date}</Text>
+        <Text>{this.state.data.location}</Text>
+        <Text>{this.state.data.description}</Text>
+      </View>
+    );
+  }
+}
