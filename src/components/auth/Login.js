d3ab69922e2c7d28a575de87d7394df91e99c054
You Song Hou <yousonghou@yahoo.com>
2019-07-09 00:19:18 -0400

    add TextInput

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 class Login extends Component{
+    constructor() {
+        super();
+        this.state = {
+          email: '',
+          password: ''
+        };
+    }
     render(){
+        const { login__input } = styles;
+
         return (
             <View>
-                <Text>Login</Text>
+                <TextInput
+                    value={this.state.email}
+                    placeholder="Email"
+                    onChangeText = {email => this.setState({ email })}
+                    style={login__input}/>
+                <Text>{this.state.email}</Text>
+                <TextInput
+                    value={this.state.password}
+                    placeholder="Password"
+                    onChangeText = {password => this.setState({ password })}
+                    style={login__input}/>
+                <Text>{this.state.password}</Text>
             </View>
         )
+    }
+}
+
+const styles = {
+    login__input:{
+        height: 40,
+        width: '100%',
+        backgroundColor: 'yellow'
     }
 }
 
