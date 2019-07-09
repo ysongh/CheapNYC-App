f16101198e2f70acb2b7399355eb2b6673218e50
You Song Hou <yousonghou@yahoo.com>
2019-07-09 00:57:46 -0400

    add a fetch request to login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 class Login extends Component{
     constructor() {
         super();
         this.state = {
           email: '',
-          password: ''
+          password: '',
+          token: ''
         };
     }
+
+    pressLogin(){
+        let url = "https://cnycserver.herokuapp.com/users/login";
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+              email: this.state.email,
+              password: this.state.password
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data)
+            this.setState({
+                token: data.token
+            });
+        })
+        .catch((err) => {
+            console.log('There was a problem with your fetch request' + err.message);
+        });
+    }
+
     render(){
-        const { login__input } = styles;
+        const { login__input, login__button } = styles;
 
         return (
             <View>
@@ -19,13 +47,15 @@ class Login extends Component{
                     placeholder="Email"
                     onChangeText = {email => this.setState({ email })}
                     style={login__input}/>
-                <Text>{this.state.email}</Text>
                 <TextInput
                     value={this.state.password}
                     placeholder="Password"
                     onChangeText = {password => this.setState({ password })}
                     style={login__input}/>
-                <Text>{this.state.password}</Text>
+                <TouchableOpacity style={login__button} onPress={() => this.pressLogin()}>
+                    <Text style={styles.deals__name}>Enter</Text>
+                </TouchableOpacity>
+                <Text>{this.state.token}</Text>
             </View>
         )
     }
@@ -35,8 +65,15 @@ const styles = {
     login__input:{
         height: 40,
         width: '100%',
-        backgroundColor: 'yellow'
-    }
+        backgroundColor: 'yellow',
+        marginBottom: 5
+    },
+    login__button:{
+        alignSelf: 'center',
+        backgroundColor: "#82cfe8",
+        borderRadius: 5,
+        padding: 10,
+    },
 }
 
 export default Login;
