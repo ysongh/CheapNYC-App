ae03187f85675239a8b372e71b5c70280bff74b8
You Song Hou <yousonghou@yahoo.com>
2019-10-13 23:48:48 -0400

    add a button with icon component

---
+++ src/components/common/ButtonWithIcon.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+const ButtonWithIcon = ({ icon, value, onPress }) => {
+    const { button__group, button__label } = styles;
+
+    return(
+        <TouchableOpacity style={button__group} onPress={onPress}>
+            <Image source={icon} />
+            <Text style={button__label}>{value}</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    button__group:{
+        display: "flex",
+        alignItems: "center",
+        paddingVertical: 12,
+    },
+    button__label:{
+        fontSize: 15
+    }
+})
+
+export default ButtonWithIcon;
