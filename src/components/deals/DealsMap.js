4d211ce0f9b3e5d27adaeb5c5689f052d5c8fcba
You Song Hou <yousonghou@yahoo.com>
2019-10-20 22:41:37 -0400

    add onPress to Marker to show the selected deal

--- src/components/deals/DealsMap.js
+++ src/components/deals/DealsMap.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import MapView from 'react-native-maps';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+import Button from '../common/Button';
+import noImage from '../../img/blue.jpeg';
 
 class DealsMap extends Component{
+    state = {
+        dealData: ""
+    };
+
+    showDeal(deal){
+        this.setState({ dealData: deal})
+    }
+
     render(){
         const deals = this.props.deals;
+        const {deals__name, deals__rightSide, deals__container, deals__price, deals__button} = styles;
 
         const markers = deals.map(deal => {
             return (
@@ -13,24 +27,65 @@ class DealsMap extends Component{
                         latitude: deal.latitude,
                         longitude: deal.longitude
                     }}
-                    title={ deal.company }
-                    description={ deal.location } />
+                    onPress={() => this.showDeal(deal)} />
             );
         });
 
         return (
-            <MapView
-                style={{ height: 500 }}
-                initialRegion={{
-                    latitude: 40.7478,
-                    longitude: -73.9560,
-                    latitudeDelta: 0.5252,
-                    longitudeDelta: 0.5242,
-                }} >
-                    { markers }
-            </MapView>
+            <View>
+                <MapView
+                    style={{ height: 420 }}
+                    initialRegion={{
+                        latitude: 40.7478,
+                        longitude: -73.9560,
+                        latitudeDelta: 0.5252,
+                        longitudeDelta: 0.5242,
+                    }} >
+                        { markers }
+                </MapView>
+                <View style={deals__container}>
+                    <View>
+                        <Image source={this.state.dealData.image ? {uri: this.state.dealData.image} : noImage} style={{width: 100, height: 100}}/>
+                    </View>
+                    <View style={deals__rightSide}>
+                        <Text style={deals__name}>{this.state.dealData.name}</Text>
+                        <Text style={deals__price}>${this.state.dealData.price === 0 ? "Free" : this.state.dealData.price}</Text>
+                        <Button
+                            buttonStyle={deals__button}
+                            textStyle={deals__name}
+                            value="See More"
+                            onPress={() => Actions.deal({dealID: this.state.dealData._id})} />
+                    </View>
+                </View>
+            </View>
         );
     };
 };
+
+const styles = StyleSheet.create({
+    deals__container:{
+        backgroundColor: "#daf2f2",
+        flexDirection: "row",
+        marginTop: 5,
+        marginBottom: 10,
+        padding: 2
+    },
+    deals__rightSide:{
+        paddingLeft: 10,
+    },
+    deals__name:{
+        fontSize: 20
+    },
+    deals__button:{
+        alignSelf: 'flex-start',
+        backgroundColor: "#f0b6b7",
+        borderRadius: 5,
+        padding: 6,
+        marginTop: 10
+    },
+    deals__price:{
+        fontSize: 20
+    }
+});
 
 export default DealsMap;
